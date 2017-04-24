ddd9ed30e02bcec19df72eab83185d16e41ab6be
naimjeem <naim36208@gmail.com>
2017-04-24 19:13:28 +0600

    update

--- api/query.js
+++ api/query.js
@@ -9,30 +9,33 @@
 //    connection.connect();
 
 var query = function(req, res){
+    
+    app.post('/', function(req, res, next){
 
-    var chat_data = {
-        to_country: data.whereto,
-        to_airport: data.toairport,
-        from_country: data.fromwhere,
-        from_airport: data.fromairport,
-        planetype: data.planetype,
-        startdate: data.startdate,
-        starttime: data.starttime,
-        returndate: 'yes/no',
-        returntime: 'yes/no',
-        email: data.email,
-        distance: data.distance,
-        estimatedhrs: data.estimatedhrs,
-        planecostperhr: data.planecostperhr,
-        estimatedcost: data.estimatedcost,
-        estimated_time: data.estimated_time
-    };
+        var chat_data = {
+            to_country: data.whereto,
+            to_airport: data.toairport,
+            from_country: data.fromwhere,
+            from_airport: data.fromairport,
+            planetype: data.planetype,
+            startdate: data.startdate,
+            starttime: data.starttime,
+            returndate: 'yes/no',
+            returntime: 'yes/no',
+            email: data.email,
+            distance: data.distance,
+            estimatedhrs: data.estimatedhrs,
+            planecostperhr: data.planecostperhr,
+            estimatedcost: data.estimatedcost,
+            estimated_time: data.estimated_time
+        };
 
-    var insert = connection.query("INSERT INTO chat SET ?", chat_data, function(err, result){
-        if(err) throw err;
-        console.log('data inserted'+insert);
+        var insert = connection.query("INSERT INTO chat SET ?", chat_data, function(err, result){
+            if(err) throw err;
+            console.log('data inserted'+insert);
+        });
+
     });
-
 };
 
 module.exports = query;
